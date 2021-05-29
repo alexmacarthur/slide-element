042b938a226f80af503a2d5d83a984a15d7ce11c
Alex MacArthur <alex@macarthur.me>
2021-05-29 13:43:49 -0500

    Use Jests's done() instead of async/await for tests.

--- src/index.test.js
+++ src/index.test.js
@@ -7,81 +7,71 @@ beforeEach(() => {
     .mockImplementation(() => 100);
 });
 
-it("removes event listeners after finishing", async () => {
-  return await new Promise((resolve) => {
-    document.body.innerHTML = `<div data-testid="content" style="display: none;">Content!</div>`;
-    const el = screen.getByTestId("content");
+it("removes event listeners after finishing", (done) => {
+  document.body.innerHTML = `<div data-testid="content" style="display: none;">Content!</div>`;
+  const el = screen.getByTestId("content");
 
-    down(el).then(() => {
-      expect(el.ontransitionend).toBeNull();
-      expect(el.ontransitioncancel).toBeNull();
+  down(el).then(() => {
+    expect(el.ontransitionend).toBeNull();
+    expect(el.ontransitioncancel).toBeNull();
 
-      return resolve();
-    });
-
-    el.ontransitionend(el);
+    done();
   });
+
+  el.ontransitionend(el);
 });
 
-it("opens element", async () => {
-  return await new Promise((resolve) => {
-    document.body.innerHTML = `<div data-testid="content" style="display: none;">Content!</div>`;
-    const el = screen.getByTestId("content");
+it("opens element", (done) => {
+  document.body.innerHTML = `<div data-testid="content" style="display: none;">Content!</div>`;
+  const el = screen.getByTestId("content");
 
-    down(el).then((opened) => {
-      expect(opened).toBe(true);
-      expect(el.style.display).toEqual("block");
+  down(el).then((opened) => {
+    expect(opened).toBe(true);
+    expect(el.style.display).toEqual("block");
 
-      return resolve();
-    });
-
-    el.ontransitionend(el);
+    done();
   });
+
+  el.ontransitionend(el);
 });
 
-it("closes element", async () => {
-  return await new Promise((resolve) => {
-    document.body.innerHTML = `<div data-testid="content">Content!</div>`;
-    const el = screen.getByTestId("content");
+it("closes element", (done) => {
+  document.body.innerHTML = `<div data-testid="content">Content!</div>`;
+  const el = screen.getByTestId("content");
 
-    up(el).then((opened) => {
-      expect(opened).toBe(false);
-      expect(el.style.display).toEqual("none");
+  up(el).then((opened) => {
+    expect(opened).toBe(false);
+    expect(el.style.display).toEqual("none");
 
-      return resolve();
-    });
-
-    el.ontransitionend(el);
+    done();
   });
+
+  el.ontransitionend(el);
 });
 
 document.body.innerHTML = `<div data-testid="content" style="display: none;">Content!</div>`;
 const el = screen.getByTestId("content");
 
-it("toggles element open", async () => {
+it("toggles element open", (done) => {
   jest
     .spyOn(HTMLDivElement.prototype, "clientHeight", "get")
     .mockImplementation(() => 0);
 
-  return await new Promise((resolve) => {
-    toggle(el).then((opened) => {
-      expect(opened).toBe(true);
+  toggle(el).then((opened) => {
+    expect(opened).toBe(true);
 
-      return resolve();
-    });
-
-    el.ontransitionend(el);
+    done();
   });
+
+  el.ontransitionend(el);
 });
 
-it("toggles element closed", async () => {
-  return await new Promise((resolve) => {
-    toggle(el).then((opened) => {
-      expect(opened).toBe(false);
+it("toggles element closed", (done) => {
+  toggle(el).then((opened) => {
+    expect(opened).toBe(false);
 
-      return resolve();
-    });
-
-    el.ontransitionend();
+    done();
   });
+
+  el.ontransitionend();
 });
